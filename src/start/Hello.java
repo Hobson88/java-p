8917674636ce79d67fc2306d6b933d42a52c8b09
Hobson88 <krawczakrobert@gmail.com>
2019-02-14 10:04:39 +0100

    START

---
+++ src/start/Hello.java
@@ -0,0 +1,14 @@
+package start;
+//psvm + enter      public static void main(String[] args) {
+//sout + enter      System.out.println();
+//CTRL+SHIFT+F10    run
+import javax.swing.*;
+
+public class Hello {
+    public static void main(String[] args) {
+       d for (int i = 0; i < 1000; i++) {
+            System.out.println("Hello World! " + i);
+            //JOptionPane.showMessageDialog(null, "FORMAT DYSKU TRWA! ");
+        }
+    }
+}
